ed687d14770a9ec7359f877a6b3645e3eb8e2e49
Dishalex <131618968+Dishalex@users.noreply.github.com>
2023-04-25 23:48:31 +0300

    Task 15 of autocheck was done

--- calculator_(15 of 15 of part-2).py
+++ calculator_(15 of 15 of part-2).py
@@ -4,12 +4,24 @@ operator = None
 wait_for_number = True
 
 
-def count(number, operation, res):
-    if
+def count():  # counting function
+    global operand, operator, result
+    if operator == '+':
+        result = result + operand
+    elif operator == '-':
+        result = result - operand
+    elif operator == '*':
+        result = result * operand
+    elif operator == '/':
+        if operand != 0:
+            result = result / operand
+        else:
+            print('Division by zero')
 
 
-while True:
-    while wait_for_number:
+def num_input():  # input number function
+    global wait_for_number, operand
+    while wait_for_number == True:
         operand = input('Input number: ')
         if operand.isdigit():
             operand = float(operand)
@@ -17,17 +29,24 @@ while True:
         else:
             print(f'{operand} is not a number. Try again.')
 
+
+def operation_input():  # operator input function
+    global operator, wait_for_number
     while not wait_for_number:
         operator = input('Input operator: ')
-        if operator == '=':
-            break
-        elif operator in '/*-+':
+        if operator in '/*-+=':
             wait_for_number = True
         else:
             print(f"{operator} is not '+' or '-' or '/' or '*'. Try again")
 
+
+num_input()  # first call of this function return operand that assigning to a result variable
+result = operand
+
+while True:  # main operational function
+    operation_input()
     if operator == '=':
         print(result)
         break
-
-    else count(operand, operator, result):
+    num_input()
+    count()
